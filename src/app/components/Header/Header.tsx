0b92b0cff68d6334858ee0ea9eeb1848fc683e08
Yoga Febriatala <yogafebriatala24@gmail.com>
2024-12-16 23:19:35 +0700

    first

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <div className="border-b border-gray-200">
+        <div className="max-w-4xl mx-auto">Header</div>
+      </div>
+    </>
+  );
+}
---
+++ src/app/pages/terbaru/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Terbaru() {
+  return (
+    <>
+      <div className="">test</div>
+    </>
+  );
+}
