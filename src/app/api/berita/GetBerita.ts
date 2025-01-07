2c296df1964888ac4ec5771550e389e6d086fbc7
Yoga Febriatala <yogafebriatala24@gmail.com>
2025-01-07 10:32:55 +0700

    fix font

--- src/app/api/berita/GetBerita.ts
+++ src/app/api/berita/GetBerita.ts
@@ -1,7 +1,15 @@
 import axiosInstance from "@/app/libs/axiosIntance";
 import { BeritaType } from "@/app/types/BeritaType";
+import { DetailBeritaType } from "@/app/types/DetailBerita";
 
 export const fetchBeritaList = async (): Promise<BeritaType[]> => {
   const response = await axiosInstance.get("/home");
   return response?.data.data.artikel_nasional;
 };
+
+export async function fetchDetailBerita(
+  slug: string
+): Promise<DetailBeritaType> {
+  const response = await axiosInstance.get(`/artikel/${slug}`);
+  return response.data.data;
+}
