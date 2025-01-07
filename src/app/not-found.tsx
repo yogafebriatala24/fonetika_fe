2c662be72fcd05c2b406950b8eb2ff8e9d33b46f
Yoga Febriatala <yogafebriatala24@gmail.com>
2025-01-07 14:28:09 +0700

    fix params

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,7 @@
 "use client";
-import React, { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import React from "react";
 
 export default function NotFoundPage() {
-  const router = useRouter();
-  useEffect(() => {
-    setTimeout(() => {
-      router.push("/");
-    }, 4000);
-  }, []);
   return (
     <div className="flex items-center justify-center min-h-screen text-white bg-secondColor">
       <div className="text-center">
