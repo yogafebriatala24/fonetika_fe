9e0c862bca5c0e76c901bef2685471f743556d9e
Yoga Febriatala <yogafebriatala24@gmail.com>
2025-01-07 11:20:39 +0700

    fix params

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["tangselupdate.nusacitateknologi.com"],
   },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
 export default nextConfig;
